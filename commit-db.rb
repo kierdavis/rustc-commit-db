cbe9ba662d7f0aaf9f78700ada658910f876c36e
Jethro Beekman <jethro@fortanix.com>
2017-04-06 16:13:46 -0700

    Get data directly from distribution manifests.
    Data in fixups that is superseded by manifests has been deleted.

--- commit-db.rb
+++ commit-db.rb
@@ -13,7 +13,7 @@
 require 'json'
 require 'net/https'
 
-class BuildCache
+class BbotCache
 	def initialize(channel)
 		@channel=channel
 		fixups=JSON.load(IO.read(fixups_file))
@@ -31,7 +31,7 @@ class BuildCache
 	def update(force=false)
 		return unless force || (File.mtime(cache_dir) <= (Time.now-86400))
 
-		$stderr.puts "Checking for updates on #{@channel}"
+		$stderr.puts "Checking for buildbot updates on #{@channel}"
 		info=Net::HTTP.get(URI("https://buildbot.rust-lang.org/json/builders/#{@channel}-dist-rustc-linux"))
 		raise IOError, "Unable to obtain builder info" if info.nil?
 
@@ -130,6 +130,126 @@ private
 	end
 end
 
+class CommitCache
+	def initialize
+		@cache=IO.foreach(cache_file).map(&:strip).to_a
+		@dirty=false
+	end
+
+	def lookup(short_commit)
+		i = @cache.bsearch_index { |lc| short_commit <= lc }
+		if i.nil? || !@cache[i].start_with?(short_commit) then
+			i||=@cache.count
+			uri=URI("https://api.github.com/repos/rust-lang/rust/commits/#{short_commit}")
+			lc=Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.get(uri, :Accept => 'application/vnd.github.VERSION.sha') }.body
+			@cache.insert(i,lc)
+			@dirty=true
+			lc
+		else
+			@cache[i]
+		end
+	end
+
+	def save
+		if @dirty
+			IO.write(cache_file,@cache.map{|s|s+"\n"}*"")
+		end
+	end
+
+	def self.instance
+		@@instance
+	end
+
+private
+	def cache_file
+		"commit_cache"
+	end
+
+	@@instance ||= CommitCache.new
+	private_class_method :new
+end
+
+class DistCache
+	def initialize(channel)
+		@channel=channel
+
+		if @channel=="stable" then
+			glob="#{cache_dir}/*.toml"
+			re=/\/channel-rust-(\d+\.\d+\.\d+)\.toml-version = "([^"]+)"$/
+			dists=rg_versions(glob,re).map{|m|[m[1],m[2]]}
+		else
+			glob="#{cache_dir}/*/channel-rust-#{@channel}.toml"
+			re=Regexp.new("/(\\d{4}-\\d{2}-\\d{2})/channel-rust-#{@channel}\.toml-version = \"([^\"]+)\"$")
+			dists=rg_versions(glob,re).map{|m|["#{@channel}-#{m[1]}",m[2]]}
+		end
+
+		@dists=Hash[dists.map{|k,v|
+			if /^\S+ \((\h{9}) \d{4}-\d{2}-\d{2}\)$/=~v then
+				v=CommitCache.instance.lookup($~[1])
+			else
+				$stderr.puts "Error parsing version: #{version}"
+				next
+			end
+			[k,v]
+		}.compact]
+	end
+
+	def rg_versions(glob,re)
+		IO.popen(["rg","-r","-F","-A1","-N","--no-heading","[pkg.rust]","--glob",glob]).each_line.map {|l| $~ if re=~l }.compact
+	end
+
+	def update(force, index)
+		return unless force || (File.mtime(cache_dir) <= (Time.now-86400))
+
+		$stderr.puts "Checking for release updates on #{@channel}"
+
+		if @channel=="stable" then
+			tomls=index.lines.map{|l|l=~Regexp.new("^/dist/(channel-rust-\\d+\\.\\d+\\.\\d+\\.toml),")&&$~[1]}.compact
+		else
+			tomls=index.lines.map{|l|l=~Regexp.new("^/dist/(\\d{4}-\\d{2}-\\d{2}/channel-rust-#{@channel}\\.toml),")&&$~[1]}.compact
+		end
+
+		count=0
+		tomls.each do |toml|
+			f="#{cache_dir}/#{toml}"
+			if not File.exists?(f) then
+				count+=1
+				begin
+					Dir.mkdir(File.dirname(f))
+				rescue Errno::EEXIST
+				end
+				data=Net::HTTP.get(URI("#{url_base}/#{toml}"))
+				raise IOError, "Unable to obtain release info" if data.nil?
+				IO.write(f,data)
+			end
+		end
+		$stderr.puts "Retrieved #{count} new releases"
+
+		File.utime(Time.now,Time.now,cache_dir)
+	end
+
+	def valid_revisions
+		@dists.values
+	end
+
+	def lookup_by_commit(commit)
+		commit=CommitCache.instance.lookup(commit)
+		@dists.select{|k,v|v==commit}.map{|k,v|k}.to_a
+	end
+
+	def latest
+		@dists.keys.sort.reverse[0]
+	end
+private
+	def cache_dir
+		"dist_toml_cache"
+	end
+
+	def url_base
+		"https://static.rust-lang.org/dist"
+	end
+end
+
 if !%w(lookup list-valid update).include?ARGV[0] then
 	$stderr.puts "Usage:"
 	$stderr.puts "	commit-db update [--force]"
@@ -144,20 +264,25 @@ case ARGV[0]
 when 'update'
 	index=Net::HTTP.get(URI("https://static.rust-lang.org/dist/index.txt"))
 	['beta','stable','nightly'].each do |channel|
-		bcache=BuildCache.new(channel)
-		bcache.update(ARGV[1]=='--force')
-		missing=bcache.check_missing(index)
-		puts "Missing buildbot versions for #{channel}: #{missing*", "}" if missing.length>0
-		$stderr.puts "Latest nightly: #{bcache.latest}" if channel=='nightly'
+		dcache=DistCache.new(channel)
+		dcache.update(ARGV[1]=='--force',index)
+		$stderr.puts "Latest dist nightly: #{dcache.latest}" if channel=='nightly'
+		#bcache=BbotCache.new(channel)
+		#bcache.update(ARGV[1]=='--force')
+		#missing=bcache.check_missing(index)
+		#puts "Missing buildbot versions for #{channel}: #{missing*", "}" if missing.length>0
+		#$stderr.puts "Latest buildbot nightly: #{bcache.latest}" if channel=='nightly'
 	end
 when 'lookup'
 	['nightly','beta','stable'].each do |channel|
-		bcache=BuildCache.new(channel)
-		puts bcache.lookup_by_commit(ARGV[1])
+		bcache=BbotCache.new(channel)
+		dcache=DistCache.new(channel)
+		puts (bcache.lookup_by_commit(ARGV[1])+dcache.lookup_by_commit(ARGV[1])).uniq
 	end
 when 'list-valid'
-	bcache=BuildCache.new(ARGV[1])
-	puts bcache.valid_revisions
+	bcache=BbotCache.new(ARGV[1])
+	dcache=DistCache.new(ARGV[1])
+	puts (bcache.valid_revisions+dcache.valid_revisions).uniq
 end
 
-
+CommitCache.instance.save
